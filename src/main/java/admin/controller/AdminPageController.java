ca67ccbd117c29b80f750a291a49649817317abe
DESKTOP-6F5N7CQ\admin <aaa110838@naver.com>
2020-10-13 13:56:21 +0900

    10/13 13:56 lsa

--- src/main/java/admin/controller/AdminPageController.java
+++ src/main/java/admin/controller/AdminPageController.java
@@ -8,9 +8,23 @@ public class AdminPageController {
 	final String command="admin.page";
 	final String getPage="/admin_main_4";
 	
+	final String command2="user.page";
+	final String getPage2="/user_main_4";
+	
 	@RequestMapping(command)
 	public String doAction() {
 		
 		return getPage;
 	}
+	
+	
+	@RequestMapping(command2)
+	public String doAction2() {
+		
+		return getPage2;
+	}
+	
+	
+	
+	
 }
